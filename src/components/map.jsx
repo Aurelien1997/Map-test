6ac0bfc3289b3fd3da853e3e91c5ab977a7f7362
Aurelien1997 <tawpo36@gmail.com>
2023-01-23 01:36:47 +0000

    add localStorage + zoom limits

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -7,31 +7,38 @@ const API_KEY = process.env.REACT_APP_PRIVATE_KEY;
 const DEFAULT_LNG = 4.834277;
 const DEFAULT_LAT = 45.763420;
 const DEFAULT_ZOOM = 12;
+const MIN_ZOOM = 10;
+const MAX_ZOOM = 15;
 
 export default function Map() {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(DEFAULT_LNG);
-  const [lat, setLat] = useState(DEFAULT_LAT);
-  const [zoom, setZoom] = useState(DEFAULT_ZOOM);
+  const [center] = useState(JSON.parse(localStorage.getItem("center")) || [DEFAULT_LNG, DEFAULT_LAT]);
+  const [zoom] = useState(JSON.parse(localStorage.getItem("zoom")) || DEFAULT_ZOOM);
 
   useEffect(() => {
     if (map.current) return; 
     map.current = new maplibregl.Map({
       container: mapContainer.current,
       style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${API_KEY}`,
-      center: [lng, lat],
-      zoom: zoom
+      center: center,
+      zoom: zoom,
+      minZoom: MIN_ZOOM,
+      maxZoom: MAX_ZOOM
     });
     map.current.addControl(new maplibregl.NavigationControl(), 'top-right');
-    new maplibregl.Marker({color: "red"})
-      .setLngLat([lng, lat])
+    new maplibregl.Marker({color:"red"})
+      .setLngLat([DEFAULT_LNG, DEFAULT_LAT])
       .addTo(map.current);
-  }, [map, lng, lat, zoom]);
-
-  return (
-    <div className="map-wrap">
-      <div ref={mapContainer} className="map" />
-    </div>
-  );
-}
+    map.current.on('move', () => {
+      localStorage.setItem("center", JSON.stringify(map.current.getCenter()));
+      localStorage.setItem("zoom", JSON.stringify(map.current.getZoom()));
+    });
+    }, [map, center, zoom]);
+    
+    return (
+      <div className="map-wrap">
+        <div ref={mapContainer} className="map" />
+      </div>
+    );
+  }
